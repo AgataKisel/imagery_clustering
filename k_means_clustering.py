d3ca0b3ec9e16f2ce8d53b0404f0edfeee8a6adb
Agata Kiseleva <agatakisel@gmail.com>
2024-06-10 16:02:20 +0300

    fuction refactoring

--- k_means_clustering.py
+++ k_means_clustering.py
@@ -1,22 +1,43 @@
 import numpy as np
 from sklearn import cluster
-from osgeo import gdal, gdal_array
-import matplotlib.pyplot as plt
-from PIL import Image
-import tarfile
+from osgeo import gdal, gdal_array 
 import os
 
-def kmeans(input_path: str, num_clusters: int, output_path: str):
+def kmeans(input_path: str, num_clusters: int, output_path: str) -> int:
+    """
+    Performs k-means clustering of any gdal-compatible raster
+    
+    Parametrs
+    ---------
+    input_path: str
+        path to source gdal-compatible raster
+
+    num_clusters: int
+        number of clusters to be created
+
+    output_path: str
+        path to create new raster in
+
+
+    Returns
+    --------
+    int 
+        0 if finished correct
+        1 if not
+
+    Description 
+    -----------
+    based on https://scikit-learn.org/stable/modules/generated/sklearn.cluster.KMeans.html
+    """
+    
     gdal.UseExceptions()
     gdal.AllRegister()
     img_ds = gdal.Open(input_path, gdal.GA_ReadOnly)
-    num_rasters = img_ds.RasterCount
     
-    img = np.zeros((img_ds.RasterYSize, img_ds.RasterXSize, num_rasters),
-                   gdal_array.GDALTypeCodeToNumericTypeCode(img_ds.GetRasterBand(1).DataType))
+    num_rasters = img_ds.RasterCount
 
-    for b in range(num_rasters):
-        img[:, :, b] = img_ds.GetRasterBand(b + 1).ReadAsArray()
+    img = imd_ds.ReadAsArray()
+    img = np.moveaxis(img, 0, -1)
     
     X = img.reshape((-1, num_rasters))
     
@@ -25,13 +46,11 @@ def kmeans(input_path: str, num_clusters: int, output_path: str):
     
     X_cluster = k_means.labels_
     X_cluster = X_cluster.reshape(img.shape[:2])
-
-    [cols, rows] = img_ds.GetRasterBand(1).ReadAsArray().shape
     
     format = "GTiff"
     driver = gdal.GetDriverByName(format)
     
-    out_data_raster = driver.Create(output_path, rows, cols, 1, gdal.GDT_Byte)
+    out_data_raster = driver.Create(output_path, img_ds.RasterYSize, img_ds.RasterXSize, 1, gdal.GDT_Byte)
     out_data_raster.SetGeoTransform(img_ds.GetGeoTransform())
     out_data_raster.SetProjection(img_ds.GetProjection())
     
@@ -39,5 +58,5 @@ def kmeans(input_path: str, num_clusters: int, output_path: str):
     out_data_raster.FlushCache() 
     del out_data_raster
 
-    return X_cluster
+    return 0
 
